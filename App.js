d059b13626e7c73559a16c03436955bcffe1994a
srilakshmi-1509 <YARAGUNI.SRILAKSHMI@CAPGEMINI.COM>
2025-04-01 19:37:29 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Browser from './Routing/Browser';
+// import UseEffect from './UseEffect';
+
+
+const App = () => {
+  return (
+    <div>
+      <h1><Browser/></h1>
+    </div>
+  )
+}
+ 
+export default App
+  
